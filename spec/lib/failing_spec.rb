aefea55441c301a82ef1789babdb1f28c0d1eb43
Otto Vehvilainen <vehvis@flowdock.com>
2015-01-28 19:11:53 +0200

    Fix

--- spec/lib/failing_spec.rb
+++ spec/lib/failing_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 describe 'spec failure' do
   it "will break the build if uncommented" do
     # Uncomment the line below (remove # from the beginning) to break the build
-    expect(false).to be true
+    # expect(false).to be true
   end
 end
