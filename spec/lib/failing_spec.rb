4f6b33a0e77c7f101b4d64bd6e10ff945dc1e1a8
Kinshuk Kale <kkale@rallydev.com>
2015-05-29 12:15:05 +0200

    Tired of this failure. Time to fix it

--- spec/lib/failing_spec.rb
+++ spec/lib/failing_spec.rb
@@ -4,6 +4,6 @@ describe 'spec failure' do
   it "will break the build if uncommented" do
     # Uncomment the line below (remove # from the beginning) to break the build
     # fixed it
-    expect(false).to be true
+    #expect(false).to be true
   end
 end
